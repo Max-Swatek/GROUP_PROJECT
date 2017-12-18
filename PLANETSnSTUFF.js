a76ea0769c4836991b2d5daa624c4b7bbc6b94df
Sarah Brown <chocobruny@gmail.com>
2017-12-17 19:20:09 -0600

    Added more planets and music

--- PLANETSnSTUFF.js
+++ PLANETSnSTUFF.js
@@ -77,6 +77,14 @@ function fillScene() {
 	var moon = drawPlanet({ x:500, y:0, z:1050, radius:planetRadius, folder:'moon'});
 	planets.push(moon);
 
+	var poke = drawPlanet({ x:650, y:0, z:1000, radius:planetRadius, folder:'PokeMap', atmosphere:true });
+	planets.push(poke);
+
+	var volcano = drawPlanet({ x:600, y:0, z:1050, radius:planetRadius, folder:'tatooine', atmosphere:true });
+	planets.push(volcano);
+
+	var alt = drawPlanet({ x:650, y:0, z:1050, radius:planetRadius, folder:'altEarth', atmosphere:true });
+	planets.push(alt);
 
 	for (const i in planets) {
 		scene.add(planets[i].base);
